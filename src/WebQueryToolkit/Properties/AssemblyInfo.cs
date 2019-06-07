e4ba1ee42dc884319560567543deab038fe212fc
Max Toro <maxtoroq@gmail.com>
2019-06-07 19:05:01 -0400

    table css tweaks

--- src/WebQueryToolkit/Properties/AssemblyInfo.cs
+++ src/WebQueryToolkit/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("http://maxtoroq.github.io/WebQueryToolkit/")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
