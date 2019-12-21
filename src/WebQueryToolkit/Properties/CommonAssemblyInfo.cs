d2f6ccc7ee680e51c6f373d755950b1203f7e196
Max Toro <maxtoroq@gmail.com>
2019-12-21 17:48:07 -0300

    version bump

--- src/WebQueryToolkit/Properties/CommonAssemblyInfo.cs
+++ src/WebQueryToolkit/Properties/CommonAssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("WebQueryToolkit")]
 [assembly: AssemblyCompany("http://maxtoroq.github.io/WebQueryToolkit/")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
