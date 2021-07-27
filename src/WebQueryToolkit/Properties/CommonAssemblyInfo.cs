d578bb5b7de335e9f87af1b8d59406b94b58bd41
Max Toro <maxtoroq@gmail.com>
2021-07-27 00:42:40 -0400

    xcst upgrade

--- src/WebQueryToolkit/Properties/CommonAssemblyInfo.cs
+++ src/WebQueryToolkit/Properties/CommonAssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyProduct("WebQueryToolkit")]
 [assembly: AssemblyCompany("http://maxtoroq.github.io/WebQueryToolkit/")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("0.24.0")]
+[assembly: AssemblyFileVersion("0.25.0")]
